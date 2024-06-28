c99427d7a8c3491a08092170a6518e5ad8d22299
krishtimil <krishtimil@gmail.com>
2024-06-29 00:14:17 +0545

    deleted some unnecessary files

--- backup_script.sh
+++
@@ -1,3 +0,0 @@
-source /home/bob/DoeceAlumniStudentPortal/venv/bin/activate
-python /home/bob/DoeceAlumniStudentPortal/manage.py dbbackup
-python /home/bob/DoeceAlumniStudentPortal/manage.py mediabackup
